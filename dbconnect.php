2f3c0b7fbff4302ffbde760c27cc6bb3e3b94778
kellyfire611@gmail.com <kellyfire611@gmail.com>
2022-08-04 13:45:58 +0000

    bổ sung chức năng Thêm mới Truyện tranh

--- dbconnect.php
+++ dbconnect.php
@@ -11,7 +11,7 @@
 // 2. username: tên tài khoản được phép truy cập vào database server
 // 3. password: mật khẩu tài khoản được phép truy cập vào database server
 // 4. database_name: tên database bạn muốn truy cập đến
-$conn = mysqli_connect('localhost', 'root', '', 'nentang_doctruyen_online') or die('Xin lỗi, database không kết nối được.');
+$conn = mysqli_connect('localhost', 'admin_doctruyen', 'Db@#123-$%^', 'admin_doctruyen') or die('Xin lỗi, database không kết nối được.');
 
 // Tùy chỉnh kết nối
 // Set charset là utf-8 đối với kết nối này. Dùng để gõ tiếng Việt, Nhật, Thái, Trung Quốc ...
